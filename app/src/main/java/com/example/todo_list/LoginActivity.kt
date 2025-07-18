7f13709036f7a240c9a54dc20e1abc20d2ecb9ed
hungvo29122003 <acczinsv5@gmail.com>
2025-07-18 22:57:23 +0700

    thêm register ui

--- app/src/main/java/com/example/todo_list/LoginActivity.kt
+++ app/src/main/java/com/example/todo_list/LoginActivity.kt
@@ -1,14 +1,23 @@
 package com.example.todo_list
 
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.todo_list.databinding.ActivityLoginBinding
+import com.google.android.material.imageview.ShapeableImageView
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var login_button: AppCompatButton
+    private lateinit var btn_back: ShapeableImageView
+    private lateinit var login_with_gg_button: AppCompatButton
+    private lateinit var login_with_fb_button: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +28,25 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        btn_back = binding.btnBack
+        login_button = binding.loginButton
+        login_with_gg_button = binding.loginWithGgButton
+        login_with_fb_button = binding.loginWithFbButton
+        btn_back.setOnClickListener{
+            finish()
+        }
+
+        login_button.setOnClickListener {
+            Toast.makeText(this, "Chức năng đang phát triển", Toast.LENGTH_SHORT).show()
+        }
+
+        login_with_gg_button.setOnClickListener {
+            Toast.makeText(this, "Chức năng đang phát triển", Toast.LENGTH_SHORT).show()
+        }
+
+        login_with_fb_button.setOnClickListener {
+            Toast.makeText(this, "Chức năng đang phát triển", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
